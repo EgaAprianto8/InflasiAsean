239099f0dce5be223f9e4fd512eb9924e6b7df35
pandupan <pangestup60@gmail.com>
2024-05-18 22:00:53 +0700

    [ADD] - Delete Indonesia

--- app/PrediksiInflasi/page.tsx
+++ app/PrediksiInflasi/page.tsx
@@ -1,11 +1,8 @@
 import Contact from '@/components/pages/LandingPage/Contact'
-import Header from '@/components/pages/LandingPage/layout/Header'
 import HeroSectionPI from '@/components/pages/PrediksiInflasi/HeroSectionPI'
 import Information from '@/components/pages/PrediksiInflasi/Information'
 import Pengenalan from '@/components/pages/PrediksiInflasi/Pengenalan'
 import AseanModal from '@/components/pages/PrediksiInflasi/TermsModal'
-import FooterPI from '@/components/pages/PrediksiInflasi/layout/FooterPI'
-import Indonesia from '@/components/pages/PrediksiInflasi/negara/grafik/Indonesia'
 import React from 'react'
 
 const page = () => {
@@ -16,7 +13,6 @@ const page = () => {
       <AseanModal/>
       <Information/>
       <Contact/>
-      <Indonesia/>
     </div>
   )
 }
